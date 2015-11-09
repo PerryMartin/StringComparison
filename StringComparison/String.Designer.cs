4b607837443b9d3b0cc0c04d92b7bea4d25db276
PerryMartin <perry.martin@ocsbstudent.ca>
2015-11-09 14:29:24 -0500

    v1.0

---
+++ StringComparison/String.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace StringComparison
+{
+    partial class frmString
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtFirstString = new System.Windows.Forms.TextBox();
+            this.txtSecondString = new System.Windows.Forms.TextBox();
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(95, 13);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(85, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Enter two strings";
+            // 
+            // txtFirstString
+            // 
+            this.txtFirstString.Location = new System.Drawing.Point(80, 43);
+            this.txtFirstString.Name = "txtFirstString";
+            this.txtFirstString.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstString.TabIndex = 1;
+            // 
+            // txtSecondString
+            // 
+            this.txtSecondString.Location = new System.Drawing.Point(80, 80);
+            this.txtSecondString.Name = "txtSecondString";
+            this.txtSecondString.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondString.TabIndex = 2;
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Location = new System.Drawing.Point(22, 46);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(26, 13);
+            this.lblFirst.TabIndex = 3;
+            this.lblFirst.Text = "First";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Location = new System.Drawing.Point(25, 86);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(44, 13);
+            this.lblSecond.TabIndex = 4;
+            this.lblSecond.Text = "Second";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(28, 150);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 5;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(130, 159);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 6;
+            // 
+            // frmString
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 204);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblFirst);
+            this.Controls.Add(this.txtSecondString);
+            this.Controls.Add(this.txtFirstString);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmString";
+            this.Text = "Comparison";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtFirstString;
+        private System.Windows.Forms.TextBox txtSecondString;
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ StringComparison/String.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 09-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-06
+ * This program displays two String
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringComparison
+{
+    public partial class frmString : Form
+    {
+        public bool StringComparing(string first, string second)
+        {
+            bool answer = false;
+
+            string firstUpper;
+            string secondUpper;
+
+            firstUpper = first.ToUpper();
+            secondUpper = second.ToUpper();
+
+            if (firstUpper.Length == secondUpper.Length)
+            {
+                if (firstUpper == secondUpper)
+                {
+                    answer = true;
+                }
+            }
+            else
+            {
+                answer = false;
+            }
+
+
+            return answer;
+        }
+
+
+        public frmString()
+        {
+          InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // Varribles & input
+
+            string Upper = this.txtFirstString.Text;
+            string Lowwer = this.txtSecondString.Text;
+            bool answer;
+            // Process
+
+            answer = StringComparing(Upper, Lowwer);
+
+            if (answer == true)
+            {
+                this.lblAnswer.Text = "They are the same.";
+            }
+            else if (answer == false)
+            {
+                this.lblAnswer.Text = "They are not the same sorry";
+            }
+
+        }
+    }
+}
